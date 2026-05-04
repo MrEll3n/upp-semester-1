3c4945f34cf023a005f5ea5e02d550cb35454e7f
MrEll3n <mail@mrellen.eu>
2026-05-04 19:18:15 +0200

    feat: added data_processor

---
+++ data_processor.cpp
@@ -0,0 +1,5 @@
+class DataProcessor {
+    bool parseData() {
+        return true;
+    }
+};
---
+++ data_processor.h
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
+const std::string PATH_MEASUREMENT = "data/mereni.csv";
+const std::string PATH_STATIONS = "data/stanice.csv";
+
+struct Measurement {
+    uint32_t station_id;
+    uint64_t ordinal;
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    float value;
+};
+
+struct Station {
+    uint32_t station_id;
+    std::string name;
+    float latitude;
+    float longtitude;
+};
+
+class DataProcessor {
+    public:
+        std::vector<Measurement> measurements;
+        std::vector<Station> stations;
+
+    bool parseData();
+
+};
